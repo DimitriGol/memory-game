e7f2259e2b32f5fe5cb04ea73ce4ba1cd9c48586
Dimitri Golyshev <dimitrigolyshev@gmail.com>
2023-03-20 18:27:36 -0400

    Add title, new-game button (w/o functionality), and styling

--- README.old.md
+++
@@ -1,2 +0,0 @@
-# memory-game
-Assignment #2 for Web Development class - Create a Memory Game App in React. 
--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='Title'>
+        <h1 className='title-text'>Memory Game</h1>
+        <button className='new-game'>New Game</button>
+      </div>
     </div>
   );
 }
