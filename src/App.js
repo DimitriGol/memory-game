16d76331ed3dff7cca63598d4306fd7370bfec45
Dimitri Golyshev <dimitrigolyshev@gmail.com>
2023-03-21 15:34:14 -0400

    Change title to 'Comp-Sci Memory Game', add credits, add minor tweaks to styling

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ function App() {
   return (
     <div className="App">
       <div className='Title'>
-        <h1 className='title-text'>Memory Game</h1>
+        <h1 className='title-text'>Comp-Sci Memory Game</h1>
+        <p>By Dimitri Golyshev</p>
         <button className='new-game'>New Game</button>
       </div>
     </div>
