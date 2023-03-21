3841bde343ddbd6833e7dcd4648f17576c16bd8c
Dimitri Golyshev <dimitrigolyshev@gmail.com>
2023-03-21 16:58:35 -0400

    Add array of card images

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
 import './App.css';
 
+//Array of all the card images
+const cardImages = [
+  { "src": "img/binary.png"},
+  { "src": "img/chatgpt.png"},
+  { "src": "img/error.jpg"},
+  { "src": "img/github.png"},
+  { "src": "img/mips.jpg"},
+  { "src": "img/stackoverflow.png"},
+  { "src": "img/tree.png"},
+  { "src": "img/tutorial.png"}
+]
+
 function App() {
   return (
     <div className="App">
@@ -7,6 +19,9 @@ function App() {
         <h1 className='title-text'>Comp-Sci Memory Game</h1>
         <p>By Dimitri Golyshev</p>
         <button className='new-game'>New Game</button>
+      </div>
+      <div className='container'>
+        /* cards will be displayed in here */
       </div>
     </div>
   );
