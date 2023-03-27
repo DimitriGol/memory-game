a2aaec7c37b1cf7f709f18f1f6886184cd6d89ee
Dimitri Golyshev <dimitrigolyshev@gmail.com>
2023-03-27 00:38:36 -0400

    Add disabled parameter to SingleCard component, affects the choiceHandler function

--- src/components/SingleCard.js
+++ src/components/SingleCard.js
@@ -1,7 +1,9 @@
-export default function SingleCard({ card, choiceHandler, flipped}) {
+export default function SingleCard({ card, choiceHandler, flipped, disabled}) {
   
   const handleClick = () => {
-    choiceHandler(card)
+    if (!disabled){
+      choiceHandler(card);
+    }
   }
   
   return(
